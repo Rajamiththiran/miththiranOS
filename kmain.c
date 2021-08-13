d791235ea14d918e063c4da92fc64d3777b77f68
Miththiran <rjmithu7@gmail.com>
2021-08-13 20:50:00 +0530

    kmain.c file modified

--- kmain.c
+++ kmain.c
@@ -1,16 +1,29 @@
 #include "io.h"
 #include "fb.h"
 #include "serial_port.h"
+#include "drivers/gdt.h"
 
-	/* The C function */
-    int sum_of_three(int arg1, int arg2, int arg3)
-    {
-        return arg1 + arg2 + arg3;
-    }
-        void run(){
-        char c[] = "Welcome";
-        write(c, 9);
+#define POSITION 0
 
-        serial_write(SERIAL_COM1_BASE, c, 7);
-    }
+void init(){
+    init_gdt();
+}
+
+void kmain()
+{
+    init();
+
+    char str[] = "Hello, Welcome to miththiranOS...!!!";
+    //determining the length of the string
+    unsigned int len = sizeof(str) / sizeof(str[0]);
+    
+    //frame builder writing
+    fb_write(POSITION, str, len);
+    
+    //serial writing
+    char str2[] = "Hello miththiran varathan";
+    serial_write(0x3F8, str2, 21);
+}
+
+
 
